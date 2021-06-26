217135df5fa4959fafdc1df3a62467f5211549e3
Antônio Rufino <antonio.rufino1802@gmail.com>
2021-06-25 23:16:15 -0300

    Utilizando Throw e IvalidArgumentException

--- exception/index.php
+++ exception/index.php
@@ -5,8 +5,14 @@ use Code\Sum;
 require __DIR__ . '/vendor/autoload.php';
 
 try{
+
   $sum = new Sum();
-  print $sum->doSum(10, 10);
+  print $sum->doSum(10, 20);
+
 }catch(\Error $e){
   print $e->getMessage();
+}catch(\Exception $e){
+  print $e->getMessage();
 }
+
+
--- exception/src/Sum.php
+++ exception/src/Sum.php
@@ -3,7 +3,17 @@
 namespace Code;
 
 Class Sum{
+
   public function doSum($num1, $num2){
-    return $num1 + $num2;
+
+    if($num2 > 10){
+      //Palavra reservada "throw" trava a tela e exibi a exception lançada ... Pode usar o \Exception
+      throw new \InvalidArgumentException("Parâmetro 2 deve ser menor ou igual a 10");
+      
+
+      return $num1 + $num2;
+    }
+
   }
+
 }
