2116fb3dca3dcae9804ab1ffed07da40369bc471
Antônio Rufino <antonio.rufino1802@gmail.com>
2021-06-25 23:48:53 -0300

    Aplicando o Finally...

--- exception/index.php
+++ exception/index.php
@@ -7,12 +7,14 @@ require __DIR__ . '/vendor/autoload.php';
 try{
 
   $sum = new Sum();
-  print $sum->doSum(10, 20);
+  print $sum->doSum(10, 10);
 
 }catch(\Error $e){
   print_r($e->getTrace());
 }catch(\Code\Exceptions\MyCustomException $e){
   print $e->getMessage();
+} finally{
+  print 'Finally...';
 }
 
 
